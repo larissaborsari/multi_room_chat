8e5c527a35789f31fb86d482376b4da40d12bbc3
Larissa Borsari <larissaborsari@hotmail.com>
2023-04-11 11:40:19 -0300

    adding basic login validations

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.initChat = function(application, req, res) {
-    res.render("chat")
+    
+    var dataForm = req.body;
+
+    req.assert('apelido', 'Name or nick is mandatory').notEmpty();
+    req.assert('apelido', 'Name or nick must have between 3 and 20 characters').len(3, 20);
+
+    var errs = req.validationErrors();
+
+    if (errs) {
+        res.render('index', {validacao : errs});
+        return;
+    }
+
+    res.render("chat");
+
 }
