cfb02df215cd71ad3d98654847de5964cb9c8c52
Larissa Borsari <larissaborsari@hotmail.com>
2023-04-12 10:15:23 -0300

    adding socket config

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 var app = require('./config/server.js');
 
 //listening door
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Server online');
 });
+
+var io = require('socket.io'). listen(server)
+
+// create web socket connection 
+io.on('connection', function(socket){
+     console.log('User is connected')
+
+     socket.on('disconnect', function() {
+        console.log('User is disconnected')
+     })
+})
